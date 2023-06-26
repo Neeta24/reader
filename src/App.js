ad9a210740da05f0a1e42fba6694abe65d46f06b
Neeta24 <neeta.sinha.24@gmail.com>
2023-06-26 13:48:13 +0600

    layout added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
+import Home from './components/Home'
+import About from './components/About'
+import ErrorPage from './components/ErrorPage'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout'
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+  
+  },
+{
+  path: '/home',
+  element: <Home></Home>,
+
+},
+{
+  path: '/about',
+  element: <About></About>,
+
+}
+])
+
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return(
+<div>
+    
+    <RouterProvider router ={router}></RouterProvider>
+  </div>
+  )
 }
 
 export default App
